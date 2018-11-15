2e3c82448fbfeb4b678086d4b723b9f76d9112c3
kabi097 <paweljanwiczynski@gmail.com>
2018-11-15 13:24:52 +0100

    Szablon Bulmy zintegrowany z aplikacją

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,74 +7,162 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <title>{{ config('app.name', 'NewsHub') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css" />
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    <div class="main-container">
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                
+                <a class="navbar-item" href="{{ url('/') }}">
+                    {{ config('app.name', 'NewsHub') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+            
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
                 </div>
-            </div>
-        </nav>
+            
+                <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                    Najnowsze Wpisy
+                    </a>
+            
+                    <a class="navbar-item">
+                    Poczekalnia
+                    </a>
+            
+                    </div>
+            
+                <div class="navbar-end">
+            
+            
+                    <div class="navbar-item has-dropdown is-hoverable" style="z-index:50;">
+                    <a class="navbar-link">
+                        TestUser
+                    </a>
+            
+                    <div class="navbar-dropdown" style="z-index:31;">
+                        <a class="navbar-item">
+                        Profil
+                        </a>
+                        <a class="navbar-item">
+                        Wiadomości
+                        </a>
+                        <a class="navbar-item">
+                        Ustawienia
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                        Zgłoś błąd
+                        </a>
+                    </div>
+                    </div>
+                
+            
+                    <div class="navbar-item">
+                    <div class="buttons">
+            
+            
+            
+            
+                        <a class="button is-primary" {{ route('register') }}>
+                        <strong>Rejestracja</strong>
+                        </a>
+                        <a class="button is-light" href="{{ route('login') }}">
+                        Login
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
-</html>
+            <div class="container">
+
+                    <small>
+                      <nav class="navbar cat" role="navigation" style="z-index:20;">
+                        <div class="navbar-brand">
+                            <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                          <span aria-hidden="true"></span>
+                          <span aria-hidden="true"></span>
+                           <span aria-hidden="true"></span>
+                    
+                        </a>  
+                      </div>
+                      
+                    <div class="navbar-menu" id="navMenu">
+                        <div class="navbar-start cat-item">
+                          
+                          <a class="navbar-item" href="test.html">
+                           Kraj
+                          </a>
+                          <a class="navbar-item">
+                           Zagranica
+                          </a>
+                          <a class="navbar-item">
+                           Irak
+                          </a>
+                          <a class="navbar-item">
+                           Technologia
+                          </a>
+                          
+                        </div>
+                      </div>
+                    </nav>
+                    </small>
+                    
+                    
+                    <!--center -->
+                    <!-- include_once 'posts_main.php'-->
+                    <!-- content-->
+                    
+                    @yield('content')
+                    
+                    
+                    
+                    
+                    </div>
+                    
+                    <footer class="footer">
+                      <div class="content has-text-centered">
+                        <p>
+                          2018 &copy NewsHUB
+                        </p>
+                      </div>
+                    </footer>
+                    
+                    
+                    
+                     <script async type="text/javascript">
+                    
+                    document.addEventListener('DOMContentLoaded', () => {
+                      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+                      if ($navbarBurgers.length > 0) {
+                        $navbarBurgers.forEach( el => {
+                          el.addEventListener('click', () => {
+                            const target = el.dataset.target;
+                            const $target = document.getElementById(target);
+                            el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+                    
+                          });
+                        });
+                      }
+                    
+                    });
+                     </script>
+                    
+                    
+                    </div>
+                    </body>
+                    
+                    </html>
