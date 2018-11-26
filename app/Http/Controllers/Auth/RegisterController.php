9e7f812560668c069dedf580ca03c2213d54f833
kabi097 <paweljanwiczynski@gmail.com>
2018-11-26 12:02:02 +0100

    Dodawanie firstname i lastname podczas rejestracji

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname'],
         ]);
     }
 
