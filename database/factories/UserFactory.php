f0da5c80990d86bb80c48194c33d412bd2f37876
kabi097 <paweljanwiczynski@gmail.com>
2018-12-01 19:27:11 +0100

    Takie same hasło dla wszystkich przykładowych userów

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => Hash::make('qwerty123'), // secret
         'image' => $faker->imageUrl(200, 200),
         'firstname' => $faker->firstName(),
         'lastname' => $faker->lastName,
