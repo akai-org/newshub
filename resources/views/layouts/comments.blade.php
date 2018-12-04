d904794df8c0fd12ecb74cc3e8af486b005e6007
kabi097 <paweljanwiczynski@gmail.com>
2018-12-04 23:09:21 +0100

    Spolszczony jquery-comments

--- resources/views/layouts/comments.blade.php
+++ resources/views/layouts/comments.blade.php
@@ -7,6 +7,24 @@ $('#post-comments').comments({
     @else 
         readOnly: true,
     @endif
+    enableHashtags: true,
+    textareaPlaceholderText: 'Napisz komentarz',
+    newestText: 'Najnowsze',
+    oldestText: 'Najstarsze',
+    popularText: 'Najlepsze', 
+    attachmentsText: 'Pokaż załączniki',
+    sendText: 'Zatwierdź',
+    replyText: 'Odpowiedź',
+    editText: 'Edytuj',
+    editedText: 'Zmodyfikowany',
+    youText: 'Ty',
+    saveText: 'Zapisz',
+    deleteText: 'Kasuj',
+    viewAllRepliesText: 'Pokaż wszystkie odpowiedzi (__replyCount__)',
+    hideRepliesText: 'Ukryj',
+    noCommentsText: 'Brak komentarzy',
+    noAttachmentsText: 'Brak załączników',
+    attachmentDropText: 'Upuść tutaj',
     getComments: function(success, error) {
         var commentsArray = [
             {!! $post->jquery_comments() !!}
