762f76fae224e34c393d3319c364a2168109df32
kabi097 <paweljanwiczynski@gmail.com>
2018-12-16 01:19:05 +0100

    Limit 20 postów na strone

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        $posts = Post::paginate(5);
+        $posts = Post::paginate(20);
         return view('posts', ['posts' => $posts]);
     }
     
